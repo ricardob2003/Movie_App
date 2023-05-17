76b141ccb5e5ad1e6ec1838834a04fc02b089be2
Ricardo Barahona <ricardodbarahona@gmail.com>
2023-05-17 15:51:14 -0600

    start

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = Movie.released
   end
 
   def show
@@ -14,12 +14,35 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params[:id])
 
-    movie_params =
-      params.require(:movie).
-        permit(:title, :description, :rating, :released_on, :total_gross)
-
+    
     @movie.update(movie_params)
 
     redirect_to @movie
   end
+
+  def new 
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to @movie
+  end
+
+    #Ask how this redirect works with movies_url 
+  def destroy 
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_url, status: :see_other
+  end
+
+private 
+    def movie_params 
+      params.require(:movie).
+      permit(:title, :description, :rating, :released_on, :total_gross,
+      :director, :duration, :image_file_name)
+    end
+
+
 end
