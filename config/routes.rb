cc529f2d9537d45dcc22b5be39ae15b9b2395dcc
Ricardo Barahona <ricardodbarahona@gmail.com>
2023-05-25 14:29:29 -0600

    start

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  resources :movies do 
+  
   root "movies#index"
+
+
+  resources :movies do 
     resources :reviews
   end 
+
+  resource :session, only: [:new, :create, :destroy]
+  get "signin" => "sessions#new"
+
+
+  resources :users
+  get "signup" => "users#new"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_18_172611) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_23_211813) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
@@ -32,6 +32,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_18_172611) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
   end
 
   add_foreign_key "reviews", "movies"
